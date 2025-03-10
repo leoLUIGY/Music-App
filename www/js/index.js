a3032b077f346ee326ced27d76e693286a543095
leoLUIGY <leonidas200999@hotmail.com>
2025-03-09 23:09:57 -0300

    first structure test

--- www/js/index.js
+++ www/js/index.js
@@ -19,11 +19,51 @@
 
 // Wait for the deviceready event before using any of Cordova's device APIs.
 // See https://cordova.apache.org/docs/en/latest/cordova/events/events.html#deviceready
-document.addEventListener('deviceready', onDeviceReady, false);
+// document.addEventListener('deviceready', onDeviceReady, false);
+document.addEventListener('deviceready',function(){
+    
+    let dirPath = cordova.file.externalRootDirectory;
+
+    function listMusicFiles(path)
+    {
+        window.resolveLocalFileSystemURL(path, function(dirEntry) {
+            let directoryReader = dirEntry.createReader();
+            directoryReader.readEntries(function(entries){
+                let musicFiles = [];
+                for(let i = 0; i< entries.length; i++)
+                {
+                    let file = entries[i];
+                    if(file.isFile && (file.name.endsWith(".mp3") || file.name.endsWith(".wav"))){
+                        //musicFiles.push(file.name);
+                        addMusicItem(file.name);
+                    }
+                }
+
+            })
+        })
+    }
+    listMusicFiles(dirPath);
+}, false);
 
 function onDeviceReady() {
     // Cordova is now initialized. Have fun!
 
-    console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
-    document.getElementById('deviceready').classList.add('ready');
+    // console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
+    // document.getElementById('deviceready').classList.add('ready');
+
+
+}
+
+
+function addMusicItem(fileName) {
+    let container = document.getElementById("body");
+
+    let itemDiv = document.createElement("div");
+    itemDiv.classList.add("itemMusic");
+
+    let link = document.createElement("a");
+    link.innerText = fileName; // Exibir apenas o nome do arquivo
+
+    itemDiv.appendChild(link);
+    container.appendChild(itemDiv);
 }
